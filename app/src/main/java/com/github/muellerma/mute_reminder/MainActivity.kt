1deac6d111e3af885c00d57f35588b53df2be03c
mueller-ma <mueller-ma@users.noreply.github.com>
2022-10-02 20:14:26 +0200

    Redesign MainActivity

--- app/src/main/java/com/github/muellerma/mute_reminder/MainActivity.kt
+++ app/src/main/java/com/github/muellerma/mute_reminder/MainActivity.kt
@@ -2,8 +2,6 @@ package com.github.muellerma.mute_reminder
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.github.muellerma.mute_reminder.databinding.ActivityMainBinding
 
@@ -13,32 +11,14 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.github.setOnClickListener {
-            "https://github.com/mueller-ma/MuteReminder".openInBrowser(this)
+        supportActionBar?.hide()
+
+        binding.settings.setOnClickListener {
+            Intent(this, PreferenceActivity::class.java).apply {
+                startActivity(this)
+            }
         }
 
         ForegroundService.changeState(this, true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.settings -> {
-                Intent(this, PreferenceActivity::class.java).apply {
-                    startActivity(this)
-                }
-                true
-            }
-            android.R.id.home -> {
-                onBackPressedDispatcher.onBackPressed()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
