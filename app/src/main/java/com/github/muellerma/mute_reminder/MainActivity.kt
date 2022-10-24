5127b7de74c941009965da8254232a30b1b7f16a
Tiago Araujo <8679058+Tgo1014@users.noreply.github.com>
2022-10-24 09:05:22 +0200

    feat: added notification permission (#41)
    Fixes #39

--- app/src/main/java/com/github/muellerma/mute_reminder/MainActivity.kt
+++ app/src/main/java/com/github/muellerma/mute_reminder/MainActivity.kt
@@ -1,24 +1,74 @@
 package com.github.muellerma.mute_reminder
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.github.muellerma.mute_reminder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val notificationPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        ::handleNotificationPermission
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         supportActionBar?.hide()
+        setupOnClickListeners()
+        checkNotificationPermissionState()
+    }
 
-        binding.settings.setOnClickListener {
-            Intent(this, PreferenceActivity::class.java).apply {
+    override fun onResume() {
+        super.onResume()
+        handleNotificationPermission(hasNotificationPermission())
+    }
+
+    private fun setupOnClickListeners() = with(binding) {
+        settings.setOnClickListener {
+            Intent(this@MainActivity, PreferenceActivity::class.java).apply {
                 startActivity(this)
             }
         }
+        notificationPermissions.setOnClickListener { openNotificationSettings() }
+    }
 
-        ForegroundService.changeState(this, true)
+    private fun checkNotificationPermissionState() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            handleNotificationPermission(true)
+            return
+        }
+        notificationPermission.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+    }
+
+    private fun handleNotificationPermission(isGranted: Boolean) {
+        binding.notificationPermissions.isVisible = !isGranted
+        if (isGranted) {
+            ForegroundService.changeState(this, true)
+        }
+    }
+
+    private fun hasNotificationPermission(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            return true
+        }
+        return hasPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+    }
+
+    private fun openNotificationSettings() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            return
+        }
+        val settingsIntent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        startActivity(settingsIntent)
     }
 }
--- app/src/main/java/com/github/muellerma/mute_reminder/Utils.kt
+++ app/src/main/java/com/github/muellerma/mute_reminder/Utils.kt
@@ -3,9 +3,13 @@ package com.github.muellerma.mute_reminder
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 private const val TAG = "Utils"
 
@@ -19,4 +23,11 @@ fun String.openInBrowser(context: Context) {
             .makeText(context, R.string.error_no_browser_found, Toast.LENGTH_SHORT)
             .show()
     }
+}
+
+fun Context.hasPermission(string: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        string
+    ) == PackageManager.PERMISSION_GRANTED
 }
