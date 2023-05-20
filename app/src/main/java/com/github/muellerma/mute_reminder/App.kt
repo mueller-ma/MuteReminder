f93a36b73cc81f38198fef3e358a950684bbad34
mueller-ma <mueller-ma@users.noreply.github.com>
2023-05-20 21:04:56 +0200

    Use dynamic colors

---
+++ app/src/main/java/com/github/muellerma/mute_reminder/App.kt
@@ -0,0 +1,11 @@
+package com.github.muellerma.mute_reminder
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
