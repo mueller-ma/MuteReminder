17fda75cad0539a595219535b13be5c7926a5e20
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-07-03 23:44:02 +0800

    [feat] Auto mute (#151)

--- app/src/main/java/com/github/muellerma/mute_reminder/Prefs.kt
+++ app/src/main/java/com/github/muellerma/mute_reminder/Prefs.kt
@@ -13,7 +13,12 @@ class Prefs(context: Context) {
         get() = sharedPrefs.getBoolean(NOTIFY_ONLY_WHEN_MUTED, true)
         set(value) = sharedPrefs.edit { putBoolean(NOTIFY_ONLY_WHEN_MUTED, value) }
 
+    var autoMute: Boolean
+        get() = sharedPrefs.getBoolean(AUTO_MUTE, false)
+        set(value) = sharedPrefs.edit { putBoolean(AUTO_MUTE, value) }
+
     companion object {
         const val NOTIFY_ONLY_WHEN_MUTED = "notify_only_when_muted"
+        const val AUTO_MUTE = "auto_mute"
     }
 }
